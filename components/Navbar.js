118beb798b5b7a28b86069d6a4e91ef6004984bb
wasim-ay <wasimbaig000@gmail.com>
2022-08-04 13:51:13 +0530

    fix shadow

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavLogo from "../public/assets/navLogo.png";
@@ -8,32 +8,58 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
+	const [shadow, setShadow] = useState(false);
 
 	const handleNav = () => {
 		setNav((prevState) => !prevState);
 	};
 
+	useEffect(() => {
+		const handleShadow = () => {
+			if (window.scrollY >= 90) {
+				setShadow(true);
+			} else {
+				setShadow(false);
+			}
+		};
+		window.addEventListener("scroll", handleShadow);
+	}, []);
+
 	return (
-		<div className="fixed w-full h-20 shadow-xl z-[100]">
+		<div
+			className={
+				shadow
+					? "fixed w-full h-20 shadow-xl z-[100]"
+					: "fixed w-full h-20 z-[100]"
+			}
+		>
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-				<Image src={NavLogo} alt="/" height="50" width="125" />
+				<Link href="/">
+					<Image
+						src={NavLogo}
+						alt="/"
+						height="50"
+						width="125"
+						className="cursor-pointer"
+					/>
+				</Link>
 				<div>
 					<ul className="hidden md:flex">
 						<Link href="/">
 							<li className="ml-10 text-sm uppercase hover:border-b">Home</li>
 						</Link>
-						<Link href="/">
+						<Link href="/#about">
 							<li className="ml-10 text-sm uppercase hover:border-b">About</li>
 						</Link>
-						<Link href="/">
+						<Link href="/#skills">
 							<li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
 						</Link>
-						<Link href="/">
+						<Link href="/#projects">
 							<li className="ml-10 text-sm uppercase hover:border-b">
 								Projects
 							</li>
 						</Link>
-						<Link href="/">
+						<Link href="/#contact">
 							<li className="ml-10 text-sm uppercase hover:border-b">
 								Contact
 							</li>
