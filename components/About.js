f6c040ec9702bf6144b13f7b060003d1ad7e5016
wasim-ay <wasimbaig000@gmail.com>
2022-08-08 21:42:30 +0530

    fix projects link

--- components/About.js
+++ components/About.js
@@ -31,9 +31,11 @@ const About = () => {
 						now spending my time building projects with React JS, Firebase, and
 						learning new technologies.
 					</p>
-					<p className="py-2 text-gray-600 underline cursor-pointer">
-						Check out some of my latest projects
-					</p>
+					<Link href="/#projects">
+						<p className="py-2 text-gray-600 underline cursor-pointer">
+							Check out some of my latest projects
+						</p>
+					</Link>
 				</div>
 				<div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
 					<Image src={AboutImg} className="rounded-xl" alt="/" />
